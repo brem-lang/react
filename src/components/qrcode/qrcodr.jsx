aa4b9837a8d3af331495a10dd9491ca835446683
jsera46 <jsera46@gmail.com>
2022-07-15 16:01:48 +0800

    Data tables

---
+++ src/components/qrcode/qrcodr.jsx
@@ -0,0 +1,45 @@
+import QRCode from "qrcode";
+import { useState } from "react";
+
+const Qrcode = () => {
+  const [url, setUrl] = useState("");
+  const [qrcode, setQrcode] = useState("");
+
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(
+      url,
+      {
+        width: 800,
+        margin: 2,
+      },
+      (err, url) => {
+        if (err) return console.error(err);
+        console.log(url);
+        setQrcode(url);
+      }
+    );
+  };
+
+  return (
+    <div className="app">
+      <h1>QR code</h1>
+      <input
+        type="text"
+        placeholder="type some text"
+        value={url}
+        onChange={(evt) => setUrl(evt.target.value)}
+      />
+      <button onClick={GenerateQRCode}>Generate</button>
+      {qrcode && (
+        <>
+          <img src={qrcode} />
+          <a href={qrcode} download="qrcode.png">
+            Download
+          </a>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Qrcode;
