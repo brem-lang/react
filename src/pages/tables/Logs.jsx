e886738d0cfc053a6fcef17618838450de65e2df
jsera46 <74752500+jsera46@users.noreply.github.com>
2022-07-27 16:48:26 +0800

    Activity logs (#14)

--- src/pages/tables/Logs.jsx
+++ src/pages/tables/Logs.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import DataTable from "react-data-table-component";
+import moment from "moment";
+
+import Spinner from "../../components/spinner/spinner.component";
+import useAuth from "../../hooks/useAuth";
+import axios from "../../api/axios";
 
 function Logs() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [filteredData, setFilteredData] = useState([]);
+
+  const { auth } = useAuth();
+
+  const getData = useCallback(async () => {
+    const config = {
+      headers: { Authorization: `Bearer ${auth.token}` },
+    };
+
+    setIsLoading(true);
+    try {
+      const res = await axios("api/get/log", config);
+      setFilteredData(res.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+
+    setIsLoading(false);
+  }, [auth, setIsLoading]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  const columns = [
+    {
+      name: "Date/Time",
+      selector: (row) => moment(row.created_at).format("lll"),
+    },
+    {
+      name: "Event",
+      selector: (row) => row.event,
+    },
+    {
+      name: "Description",
+      selector: (row) => row.description,
+    },
+    {
+      name: "Document Series Number",
+      selector: (row) => row.properties["Document Series Number"],
+    },
+  ];
+
   return (
     <div className="content-wrapper">
       <div className="content-header">
@@ -31,37 +81,18 @@ function Logs() {
           <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
               <div className="card">
-                <div className="card-header">
-                </div>
                 <div className="card-body">
-                  <table
-                    id="example1"
-                    className="table table-bordered table-striped"
-                  >
-                    <thead>
-                      <tr>
-                        <th>User Name</th>
-                        <th>Role</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Trident</td>
-                        <td>Internet Explorer 4.0</td>
-                        <td>
-                          <i className="fas fa-pen"></i>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Trident</td>
-                        <td>Internet Explorer 4.0</td>
-                        <td>
-                          <i className="fas fa-pen"></i>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  {isLoading ? (
+                    <Spinner />
+                  ) : (
+                    <DataTable
+                      columns={columns}
+                      data={filteredData}
+                      pagination
+                      selectableRowsHighlight
+                      highlightOnHover
+                    />
+                  )}
                 </div>
               </div>
             </div>
