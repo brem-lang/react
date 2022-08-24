dccbc17ab0f893329c305c2666d8046985c858cd
brem-lang <patrickbarcelo17@gmail.com>
2022-08-24 15:11:26 +0800

    handover

--- src/pages/handover/ReceiveForm.jsx
+++ src/pages/handover/ReceiveForm.jsx
@@ -4,36 +4,29 @@ import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "../../api/axios";
 import Swal from "sweetalert2";
-import RedirectError from "../../routes/RedirectError";
-import useAuth from "../../hooks/useAuth";
-const initialValue = {
-  name: "",
-};
+import { QrReader } from "react-qr-reader";
+// const initialValue = {
+//   name: "",
+// };
 function ReceiveForm() {
   const [isSuccess, setIsSuccess] = useState(false);
-  const [dataField, setDataField] = useState(initialValue);
+  // const [dataField, setDataField] = useState(initialValue);
   const [selected, setSelected] = useState();
   const [approvalDept, setApprovalDept] = useState();
+  const [isScanner, setIsScanner] = useState(false);
+  const [data, setData] = useState();
   const navigate = useNavigate();
   const location = useLocation();
-  console.log(approvalDept);
-  const onChangeHandler = (e) => {
-    const { name, value } = e.target;
-    setDataField({ ...dataField, [name]: value });
-  };
+  // const onChangeHandler = (e) => {
+  //   const { name, value } = e.target;
+  //   setDataField({ ...dataField, [name]: value });
+  // };
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("document_series_no", location.state.document_series_no);
-    formData.append("person", dataField.name);
+    formData.append("person", data);
     formData.append("department", selected);
-    if (selected === undefined) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Please Select Department",
-      });
-    }
     try {
       const res = await axios.post("/api/receiveForm", formData);
       if (res.data.success === true) {
@@ -41,13 +34,11 @@ function ReceiveForm() {
       }
     } catch (err) {
       console.log(err.response);
-      // switch (err.code) {
-      //   case "ERR_BAD_REQUEST":
-      //     // return redirectError();
-      //     console.log(err);
-      //   default:
-      //     return console.log(err, "default");
-      // }
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Pls Fill up the Fields",
+      });
     }
   };
 
@@ -83,92 +74,152 @@ function ReceiveForm() {
     navigate(-2);
   };
 
+  const openScanner = (e) => {
+    setIsScanner(true);
+    setData();
+  };
+
+  const closeScanner = (e) => {
+    setIsScanner(false);
+  };
   return (
-    <div
-      className="hold-transition login-page"
-      style={{ backgroundColor: "white" }}
-    >
-      <div className="login-box">
-        <div className="login-logo">
-          <img src={Logo} alt="AdminLTE Logo" width="400" height="120" />
-        </div>
-        {/* /.login-logo */}
-        {isSuccess ? (
-          <>
-            <h1 style={{ fontSize: 50, textAlign: "center" }}>Success!!</h1>
-            <button
-              onClick={() => close()}
-              style={{ marginLeft: 150, fontSize: 15 }}
-              type="button"
-              className="btn btn-outline-info"
-            >
-              Close
-            </button>
-          </>
-        ) : (
-          <div className="card">
-            <div className="card-body login-card-body">
-              <p className="login-box-msg">Receive Form </p>
-              <form onSubmit={(e) => handleSubmit(e)}>
-                <div className="input-group mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Name"
-                    name="name"
-                    value={dataField.name}
-                    onChange={(e) => onChangeHandler(e)}
-                    autoComplete="off"
-                    required
-                  />
-                  <div className="input-group-append">
-                    <div className="input-group-text">
-                      <span className="fas fa-building"></span>
-                    </div>
-                  </div>
-                </div>
+    <>
+      {isScanner === true ? (
+        <>
+          <h1
+            style={{
+              marginTop: "20px",
+              // marginLeft: "750px",
+              marginBottom: "-50px",
+              textAlign: "center",
+            }}
+          >
+            SCAN QR CODE HERE
+          </h1>
+          <div
+            style={{
+              marginLeft: "755px",
+              marginTop: "-10px",
+              width: "20%",
+              marginBottom: "-300px",
+            }}
+          >
+            <QrReader
+              onResult={(result, error) => {
+                if (!!result) {
+                  setData(result?.text);
+                }
 
-                <div className="input-group mb-3">
-                  <select
-                    className="form-control"
-                    value={selected}
-                    onChange={(e) => setSelected(e.target.value)}
-                  >
-                    <option selected="selected">Select Department</option>
-                    {approvalDept?.map((data) => (
-                      <option value={data.department}>{data.department}</option>
-                    ))}
-                  </select>
-
-                  <div className="input-group-append">
-                    <div className="input-group-text">
-                      <span className="fas fa-building"></span>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-6">
-                    <button type="submit" class="btn btn-success btn-block">
-                      Submit
-                    </button>
-                  </div>
-                  <div class="col-6">
-                    <button
-                      onClick={() => close()}
-                      class="btn btn-primary btn-block"
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-            {/* /.login-card-body */}
+                if (!!error) {
+                  console.info(error);
+                }
+              }}
+            />
+            {data === undefined ? null : <>{closeScanner()}</>}
           </div>
-        )}
-      </div>
-    </div>
+        </>
+      ) : (
+        <div
+          className="hold-transition login-page"
+          style={{ backgroundColor: "white" }}
+        >
+          <div className="login-box">
+            <div className="login-logo">
+              <img src={Logo} alt="AdminLTE Logo" width="400" height="120" />
+            </div>
+            {/* /.login-logo */}
+            {isSuccess ? (
+              <>
+                <h1 style={{ fontSize: 50, textAlign: "center" }}>Success!!</h1>
+                <button
+                  onClick={() => close()}
+                  style={{ marginLeft: 150, fontSize: 15 }}
+                  type="button"
+                  className="btn btn-outline-info"
+                >
+                  Close
+                </button>
+              </>
+            ) : (
+              <div className="card">
+                <div className="card-body login-card-body">
+                  <p className="login-box-msg">Handover Form</p>
+                  <form
+                  // onSubmit={(e) => handleSubmit(e)}
+                  >
+                    <div className="input-group mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                        name="name"
+                        value={data}
+                        autoComplete="off"
+                        readOnly
+                      />
+                      <div className="input-group-append">
+                        <div className="input-group-text">
+                          <span className="fas fa-user"></span>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="input-group mb-3">
+                      <select
+                        className="form-control"
+                        value={selected}
+                        onChange={(e) => setSelected(e.target.value)}
+                        required
+                      >
+                        <option selected="selected">Select Department</option>
+                        {approvalDept?.map((data) => (
+                          <option value={data.department}>
+                            {data.department}
+                          </option>
+                        ))}
+                      </select>
+
+                      <div className="input-group-append">
+                        <div className="input-group-text">
+                          <span className="fas fa-building"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                  <div class="row">
+                    <div class="col-4">
+                      <button
+                        class="btn btn-success btn-block"
+                        onClick={(e) => handleSubmit(e)}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                    <div class="col-4">
+                      <button
+                        class="btn btn-success btn-block"
+                        onClick={() => openScanner()}
+                      >
+                        Scan QR
+                      </button>
+                    </div>
+                    <div class="col-4">
+                      <button
+                        onClick={() => close()}
+                        class="btn btn-primary btn-block"
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                {/* /.login-card-body */}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
